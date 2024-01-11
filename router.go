d2ff5ed27520c01b9cc78fc82062050f8f167766
Javod Mutalliboev <javodmutalliboev@gmail.com>
2024-01-11 23:41:14 +0500

    some change

--- router.go
+++ router.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"net/http"
-	"os"
 
 	"Tahlilchi.uz/admin"
 	"Tahlilchi.uz/routerFuncs"
@@ -12,10 +11,10 @@ import (
 func Router() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", routerFuncs.Root).Methods("GET").Schemes(os.Getenv("SCHEMES")) // add .Host(os.Getenv("HOST")) in the end
+	r.HandleFunc("/", routerFuncs.Root).Methods("GET") // .Schemes(os.Getenv("SCHEMES"))  add .Host(os.Getenv("HOST")) in the end
 
 	adminRouter := r.PathPrefix("/admin").Subrouter()
-	adminRouter.HandleFunc("/login", admin.Login).Methods("POST").Schemes(os.Getenv("SCHEMES"))
+	adminRouter.HandleFunc("/login", admin.Login).Methods("POST") // .Schemes(os.Getenv("SCHEMES"))
 
 	http.ListenAndServe(":8080", r)
 }
