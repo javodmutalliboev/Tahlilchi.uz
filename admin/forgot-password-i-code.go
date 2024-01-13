03d8c5d37d9a6463a12c1bfb9c6f1de3c222750b
Javod Mutalliboev <javodmutalliboev@gmail.com>
2024-01-13 08:14:00 +0500

    some progress

---
+++ admin/forgot-password-i-code.go
@@ -0,0 +1,7 @@
+package admin
+
+import "net/http"
+
+func ForgotPasswordICode(w http.ResponseWriter, r *http.Request) {
+
+}
--- router.go
+++ router.go
@@ -16,6 +16,7 @@ func Router() {
 	adminRouter := r.PathPrefix("/admin").Subrouter()
 	adminRouter.HandleFunc("/login", admin.Login).Methods("POST") // .Schemes(os.Getenv("SCHEMES"))
 	adminRouter.HandleFunc("/forgot-password/email", admin.ForgotPasswordEmail).Methods("POST")
+	adminRouter.HandleFunc("/forgot-password/i_code", admin.ForgotPasswordICode).Methods("POST")
 
 	http.ListenAndServe(":8080", r)
 }
