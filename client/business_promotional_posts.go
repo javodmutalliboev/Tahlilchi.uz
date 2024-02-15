132b703590037b836e2c485bea28361bdffee966
Javod Mutalliboev <javodmutalliboev@gmail.com>
2024-02-15 17:25:26 +0500

    client get business promotional post list response api updated

--- client/business_promotional_posts.go
+++ client/business_promotional_posts.go
@@ -1,72 +1,81 @@
 package client
 
 import (
-	"log"
+	"fmt"
 	"net/http"
-	"strconv"
 
 	"Tahlilchi.uz/db"
+	"Tahlilchi.uz/model"
 	"Tahlilchi.uz/response"
-	"github.com/lib/pq"
+	"Tahlilchi.uz/toolkit"
 )
 
 func getBusinessPromotionalPosts(w http.ResponseWriter, r *http.Request) {
-	// Parse the page number from the query parameters
-	pageStr, ok := r.URL.Query()["page"]
-	if !ok || len(pageStr[0]) < 1 {
-		log.Printf("%v: Url Param 'page' is missing. Setting default value to 1.", r.URL)
-		pageStr = []string{"1"}
+	// get page, limit
+	page, limit, err := toolkit.GetPageLimit(r)
+	if err != nil {
+		err := fmt.Errorf("error getting page and limit: %v", err)
+		toolkit.LogError(r, err)
+		response.Res(w, "error", http.StatusBadRequest, err.Error())
+		return
 	}
-	page, _ := strconv.Atoi(pageStr[0])
 
-	// Parse the limit from the query parameters
-	limitStr, ok := r.URL.Query()["limit"]
-	if !ok || len(limitStr[0]) < 1 {
-		log.Printf("%v: Url Param 'limit' is missing. Setting default value to 10.", r.URL)
-		limitStr = []string{"10"}
-	}
-	limit, _ := strconv.Atoi(limitStr[0])
-
-	// Calculate the starting index
-	var start = (page - 1) * limit
-
+	// open a database connection
 	database, err := db.DB()
 	if err != nil {
-		log.Printf("%v: error: %v", r.URL, err)
-		response.Res(w, "error", http.StatusInternalServerError, "server error")
+		err := fmt.Errorf("error opening a database connection: %v", err)
+		toolkit.LogError(r, err)
+		response.Res(w, "error", http.StatusInternalServerError, err.Error())
 		return
 	}
 	defer database.Close()
 
-	rows, err := database.Query("SELECT * FROM business_promotional_posts WHERE archived = false AND completed = true ORDER BY id DESC LIMIT $1 OFFSET $2", limit, start)
+	// get business promotional list response
+	var bppListResponse model.BusinessPromotionalPostListResponse
+
+	// get business promotional posts from the database: select id, title_latin, description_latin, title_cyrillic, description_cyrillic, videos, updated_at where archived is false and completed is true order by id
+	// perform a database query: table is business_promotional_posts
+	rows, err := database.Query("SELECT id, title_latin, description_latin, title_cyrillic, description_cyrillic, videos, updated_at FROM business_promotional_posts WHERE archived = false AND completed = true ORDER BY id LIMIT $1 OFFSET $2", limit, (page-1)*limit)
 	if err != nil {
-		log.Printf("%v: error: %v", r.URL, err)
-		response.Res(w, "error", http.StatusInternalServerError, "server error")
+		err := fmt.Errorf("error querying the database: %v", err)
+		toolkit.LogError(r, err)
+		response.Res(w, "error", http.StatusInternalServerError, err.Error())
 		return
 	}
 	defer rows.Close()
 
-	var bpPosts []bpPost
+	// get business promotional posts
+	var bppList []model.BusinessPromotionalPost
 	for rows.Next() {
-		var bpp bpPost
-		err := rows.Scan(&bpp.ID, &bpp.TitleLatin, &bpp.DescriptionLatin, &bpp.TitleCyrillic, &bpp.DescriptionCyrillic, pq.Array(&bpp.Videos), &bpp.CoverImage)
+		var bpp model.BusinessPromotionalPost
+		err := rows.Scan(&bpp.ID, &bpp.TitleLatin, &bpp.DescriptionLatin, &bpp.TitleCyrillic, &bpp.DescriptionCyrillic, &bpp.Videos, &bpp.UpdatedAt)
 		if err != nil {
-			log.Printf("%v: error: %v", r.URL, err)
-			response.Res(w, "error", http.StatusInternalServerError, "server error")
+			err := fmt.Errorf("error scanning the database: %v", err)
+			toolkit.LogError(r, err)
+			response.Res(w, "error", http.StatusInternalServerError, err.Error())
 			return
 		}
-		bpPosts = append(bpPosts, bpp)
+		bppList = append(bppList, bpp)
 	}
 
-	response.Res(w, "success", http.StatusOK, bpPosts)
-}
+	if page > 1 {
+		bppListResponse.Previous = true
+	}
 
-type bpPost struct {
-	ID                  int      `json:"id"`
-	TitleLatin          string   `json:"title_latin"`
-	DescriptionLatin    string   `json:"description_latin"`
-	TitleCyrillic       string   `json:"title_cyrillic"`
-	DescriptionCyrillic string   `json:"description_cyrillic"`
-	Videos              []string `json:"videos"`
-	CoverImage          []byte   `json:"cover_image"`
+	var count int
+	// count by id
+	err = database.QueryRow("SELECT COUNT(id) FROM business_promotional_posts WHERE archived = false AND completed = true").Scan(&count)
+	if err != nil {
+		err := fmt.Errorf("error querying the database: %v", err)
+		toolkit.LogError(r, err)
+		response.Res(w, "error", http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	if count > page*limit {
+		bppListResponse.Next = true
+	}
+
+	bppListResponse.BPPList = bppList
+	response.Res(w, "success", http.StatusOK, bppListResponse)
 }
